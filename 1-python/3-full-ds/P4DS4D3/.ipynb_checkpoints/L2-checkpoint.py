ca09e6f6a15c5842b74135669c26a813dafd78b1
mnpawan <mnpawan@gmail.com>
2025-12-11 22:38:18 +0530

    updated

---
+++ 1-python/3-full-ds/P4DS4D3/.ipynb_checkpoints/L2-checkpoint.py
@@ -0,0 +1,7 @@
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+image = img.imread('MySamplePlot.png')
+
---
+++ 1-python/3-full-ds/P4DS4D3/L1.py
@@ -0,0 +1,3 @@
+with open('Colors.txt', 'r') as file_contents:
+    print('Colors.txt content:\n' + file_contents.read())
+    
