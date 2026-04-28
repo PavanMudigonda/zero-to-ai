577df9ee348fd353921b5f4e83b188d2b9ed0dc1
PavanMudigondaTR <NagaPavanKumar.Mudigonda@thomsonreuters.com>
2026-04-28 08:54:18 -0400

    updated

--- next-docs/src/app/FilteredLayout.tsx
+++ next-docs/src/app/FilteredLayout.tsx
@@ -1,28 +1,8 @@
 "use client";
-import React, { useMemo } from 'react';
-import { usePathname } from 'next/navigation';
+
+import React from 'react';
 import { Layout } from 'nextra-theme-docs';
 
 export default function FilteredLayout({ pageMap, children, ...props }: any) {
-  const pathname = usePathname();
-
-  const filteredPageMap = useMemo(() => {
-    if (!pathname) return pageMap;
-    const parts = pathname.split('/').filter(Boolean);
-    const topLevel = parts[0];
-    
-    if (!topLevel) return pageMap;
-
-    return pageMap.map((item: any) => {
-      if (item && item.kind === 'Folder' && item.name !== topLevel) {
-        return { ...item, children: [] };
-      }
-      if (item && item.children && item.name !== topLevel && topLevel !== "demo") {
-        return { ...item, children: [] };
-      }
-      return item;
-    });
-  }, [pageMap, pathname]);
-
-  return <Layout pageMap={filteredPageMap} {...props}>{children}</Layout>;
+  return <Layout pageMap={pageMap} {...props}>{children}</Layout>;
 }
